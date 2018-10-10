5280bb6af982c83c8304542151a4045ef4659d54
swapnavad <swapna.vad@ge.com>
2018-10-10 11:44:07 -0700

    Update dx-smoketest-expect.sh

--- scripts/dx-smoketest-expect.sh
+++ scripts/dx-smoketest-expect.sh
@@ -8,4 +8,4 @@ curl -O https://raw.githubusercontent.com/PredixDev/data-exchange-simulator/mast
 chmod 755 dx-simulator.expect
 #expect dx-simulator.expect ${githubUrl}/${repo}/${branchName}/scripts --skip-setup  uswestsmoketestedisonbas https://4f6d1d4b-c6a3-48e9-8963-18656ae191f0.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token 51bd1e37-0872-48dd-86a6-5644cb0877e2
 
-expect dx-simulator.expect https://raw.githubusercontent.com/PredixDev/data-exchange-simulator/master/scripts --skip-setup ${env.instancePrefix} ${env.UAA_ISSUREID} ${env.TS_ZONE}
+expect dx-simulator.expect https://raw.githubusercontent.com/PredixDev/data-exchange-simulator/master/scripts ${env.instancePrefix} ${env.UAA_ISSUREID} ${env.TS_ZONE}
